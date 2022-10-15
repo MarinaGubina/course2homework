de3a6f33c3ddd04ef800b513dca4d350f838f817
gubina <gubinamarina1998@gmail.com>
2022-10-15 17:14:17 +0300

    Исключения. Домашнее задание 2

--- src/data/Data.java
+++ src/data/Data.java
@@ -8,9 +8,9 @@ public class Data {
 
         public static boolean checkData(String login, String password, String confirmPassword){
             try {
-                if (checkLogin(login) == true && checkPassword(password,confirmPassword) == true) {
+                checkLogin(login);
+                checkPassword(password,confirmPassword);
                     return true;
-                }
             } catch (WrongPasswordException | WrongLoginException e){
                 System.out.println("Произошла ошибка " + e);
             }
@@ -21,23 +21,17 @@ public class Data {
             return value.matches("\\w*");
         }
 
-        public static boolean checkLogin(String login) throws WrongLoginException {
+        public static void checkLogin(String login) throws WrongLoginException {
             if (login.length() > 20 || login.length() < 1 ||
                     login == null || !containsAllowedSymbols(login)) {
                 throw new WrongLoginException("Некорректный логин!");
             }
-            else{
-                return true;
-            }
         }
-        public static boolean checkPassword(String password, String confirmPassword) throws WrongPasswordException {
+        public static void checkPassword(String password, String confirmPassword) throws WrongPasswordException {
             if (password.length() > 20 || password.length() < 1 ||
                     password == null ||
                     !containsAllowedSymbols(password) || !password.equals(confirmPassword)) {
                 throw new WrongPasswordException("Некорректный пароль!");
-            }
-            else{
-                return true;
             }
         }
 }
--- src/data/Main.java
+++ src/data/Main.java
@@ -3,6 +3,6 @@ package data;
 public class Main {
     public static void main(String[] args){
 
-        System.out.println(Data.checkData("Rg_25_03hth","gg_1234","gg_1234"));
+        System.out.println(Data.checkData("Rg_25_03hth","gg_123","gg_1234"));
     }
 }
